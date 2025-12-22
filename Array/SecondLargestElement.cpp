c3f2b6e1d5a4dd928a4c7fe93cc9481b19dbb306
Gaurav <134674802+Gauarv146@users.noreply.github.com>
2025-12-22 11:08:31 +0530

    Add files via upload

---
+++ Array/SecondLargestElement.cpp
@@ -0,0 +1,32 @@
+// Second Largest Element in an Array without sorting
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int secondLargest(int arr[], int n) {
+    if (n < 2) return -1;
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > secondLargest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    return (secondLargest == INT_MIN) ? -1 : secondLargest;
+}
+
+int main() {
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << secondLargest(arr, n);
+    return 0;
+}
